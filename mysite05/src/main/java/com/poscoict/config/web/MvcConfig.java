22ca16c1df24d92e666d194518dd1e66af4a38c1
fascinate98 <fascinate982@naver.com>
2022-02-10 12:08:06 +0900

    mysite05 #first

--- mysite05/src/main/java/com/poscoict/config/web/MvcConfig.java
+++ mysite05/src/main/java/com/poscoict/config/web/MvcConfig.java
@@ -1,18 +1,26 @@
 package com.poscoict.config.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
+@PropertySource("classpath:com/poscoict/mysite/config/web/error.properties")
 public class MvcConfig extends WebMvcConfigurerAdapter {
 	
+	
+	@Autowired
+	private Environment env;
 	//View Resolver
 	@Bean
 	public ViewResolver viewResolver() {
@@ -20,14 +28,23 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 		viewResolver.setViewClass(JstlView.class);
 		viewResolver.setPrefix("/WEB-INF/views/");
 		viewResolver.setSuffix(".jsp");
+		
 		return viewResolver;
 	}
 
 	//Message Converter
 	
 	//Defalut - 서블릿 컨테이너(tomcat)의 DefaultServlet 위임(delegate) Handler
+//	@Override
+//	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+//		configurer.enable();
+//	}
+	
 	@Override
-	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-		configurer.enable();
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		
+		registry.addResourceHandler(env.getProperty("fileupload.resourceMapping")).addResourceLocations("/error/404");
 	}
+	
+	
 }
